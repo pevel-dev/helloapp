85d36dd51afdbcb4d351db2c6ccc7d5d54bd9778
Pavel Shemetov <52889171+pevel-dev@users.noreply.github.com>
2022-11-05 18:55:59 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloapp_black
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Ivan Domashnikh!!");
+        }
+    }
+}
